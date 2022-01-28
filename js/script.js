a233fc54fba01440e5405edc9b00cae0de862b0c
illupovero <davideluporini1996@gmail.com>
2022-01-28 16:44:28 +0100

    added js objects

--- js/script.js
+++ js/script.js
@@ -1,6 +1,28 @@
+Vue.config.devtools = true;
+
 var app = new Vue({
     el: '#root',
     data: {
-
+        todos: [{
+                todo: 'comprare il latte',
+                done: true,
+            },
+            {
+                todo: 'comprare il latte',
+                done: true,
+            },
+            {
+                todo: 'comprare il latte',
+                done: true,
+            },
+            {
+                todo: 'comprare il latte',
+                done: true,
+            },
+            {
+                todo: 'comprare il latte',
+                done: true,
+            },
+        ]
     }
 })
